fcecb33b5c8a39230e54690dea55d2bacf7ef08f
jmbeach <jaredbeachdesign@gmail.com>
2016-12-17 15:11:40 -0600

    init

---
+++ server.js
@@ -0,0 +1,59 @@
+/**
+ * Title:
+ * Description:
+ */
+
+// Get process enviornment for Heroku
+process.env.PWD = process.cwd();
+
+// #region MODULES
+var express = require("express"),
+  app = express(),
+  config = require("config"),
+  dateIdeas = require("date-ideas"),
+  port = process.env.PORT || config.get("Debug.port"),
+  env = process.env.NODE_ENV || 'development'; // process.env.NODE_ENV determines whether this is the heroku app
+// #endregion
+
+// #region SERVER_CONFIG
+app.enable('trust proxy');
+
+//Files in the public folder are served staticly
+app.all('*', function (req, res, next) {
+    app.use(express.static(process.env.PWD + '/public'));
+    app.use('/bower_components', express.static(process.env.PWD + '/bower_components'));
+    next();
+});
+
+var server = app.listen(port, function () {
+    console.log('Listening on port %d', server.address().port);
+});
+// #endregion
+
+// #region MAIN_ROUTE
+app.get("(/[a-zA-Z0-9(%20)]+)+", function (req, res) {
+    console.log(req.url);
+    console.log("sending home page indirectly");
+    if (req.url.toUpperCase() == "/RANDOMIDEA") {
+      dateIdeas.randomIdea(function(err, data) {
+        if (err) {
+          res.send(err);
+        }
+        else {
+          res.send(data);
+        }
+      })
+    }
+    else {
+      res.sendFile(process.env.PWD + '/public/index.html');
+    }
+});
+// #endregion
+
+// #region REQUESTS
+// #region GET
+// #endregion
+
+// #region POST
+// #endregion
+// #endregio
